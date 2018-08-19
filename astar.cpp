7d1ee3492294b2cd8d882c5659657c2a0373ea6a
anishkelkar <anish.kelkar@gmail.com>
2018-08-20 00:56:00 +0530

    deque doesn't work

--- astar.cpp
+++ astar.cpp
@@ -25,9 +25,9 @@ class AstarQ{
 		int parent_node;
 
 		AstarQ(int _cn, ll _f, ll _g,ll _pn):current_node(_cn),fcost(_f),parent_node(_pn),gcost(_g){}
-		AstarQ(const AstarQ& elem):current_node(elem.current_node),fcost(elem.current_node),parent_node(elem.parent_node),gcost(elem.gcost){}
+		AstarQ(const AstarQ& elem):current_node(elem.current_node),fcost(elem.fcost),parent_node(elem.parent_node),gcost(elem.gcost){}
 
-		friend ostream& operator<<(ostream& o, AstarQ elem){
+		friend ostream& operator<<(ostream& o, const AstarQ& elem){
 			o << elem.current_node << " , " << elem.fcost << " , " << elem.gcost << " , " << elem.parent_node;
 			return o;
 		}
@@ -38,6 +38,15 @@ struct Astar_compare{
 		return a.fcost > b.fcost;
 	}
 };
+
+void printQ(auto pq){
+	cout << "Printing the queue" << endl;
+	while(!pq.empty()){
+		cout << pq.top() << endl;
+		pq.pop();
+	}
+	cout << "done" << endl;
+}
 
 int main(){
 	int n;
@@ -80,10 +89,10 @@ int main(){
             }
         }
     }
-	// for(auto e: h_value){
-	// 	cout << e << " ";
-	// }
-	// cout << endl;
+	for(auto e: h_value){
+		cout << e << " ";
+	}
+	cout << endl;
 	
 	visited = vll(n,0);
     vll unlocked(n,0);
@@ -101,7 +110,7 @@ int main(){
     // number of nodes with keys
     int q;
     cin >> q;
-    while(q--){
+    while(q--){	
     	int parent_node;
     	cin >> parent_node;
     	int node_key_frequency;
@@ -112,19 +121,23 @@ int main(){
     		keys[parent_node][x] = 1;
     	}
     }
-	for(int i = 0 ; i < n ; i++){
-		cout << unlocked[i] << " ";
-	}
-	cout << endl;
+	// for(int i = 0 ; i < n ; i++){
+	// 	cout << unlocked[i] << " ";
+	// }
+	// cout << endl;
 	
-	for(int i = 0 ; i < n ; i++){
-		cout << visited[i] << " ";
-	}
-	cout << endl;
+	// for(int i = 0 ; i < n ; i++){
+	// 	cout << visited[i] << " ";
+	// }
+	// cout << endl;
 
 	priority_queue< AstarQ, vector<AstarQ> ,Astar_compare> apq;
-	cout << h_value[0] << endl;	
+	// cout << h_value[0] << endl;		
+	// cout << SOURCE << endl;
+	// cout << h_value[SOURCE] << endl;
 	AstarQ init = AstarQ(SOURCE,h_value[SOURCE],0,-1);
+	// cout << init << endl;
+	// cout << init.current_node <<  " " << init.fcost << " " << init.gcost << " " << init.parent_node << endl; 
 	apq.push(init);
 	deque<ll> path;
 	int current_node  = -1;
@@ -135,15 +148,15 @@ int main(){
 		ll gcost = apq.top().gcost;
 		int parent_node = apq.top().parent_node;
 		
-		cout << apq.top() << endl;
+		cout << apq.top() <<  " " << apq.size() << endl;
 
 		
 		
 		
 		if(unlocked[current_node] == 1){
-			if(apq.top().current_node != SOURCE){
-				if(path.back() != apq.top().parent_node){
-					while(!path.empty() && path.back() != apq.top().parent_node){
+			if(current_node != SOURCE){
+				if(path.back() != parent_node){
+					while(!path.empty() && path.back() != parent_node){
 						path.pop_back();
 					}
 				}				
@@ -162,15 +175,18 @@ int main(){
 				if(graph[current_node][i] != GMAX){
 					ll next_node_gcost = gcost + graph[current_node][i];
 					if((visited[i] == 0) || (visited[i] != 0 && unlocked[i] == 1)){
-						cout << i << " ";
+						cout << i << " inserted ";
 						apq.push(AstarQ(i,next_node_gcost + h_value[i],next_node_gcost,current_node));				
 					}
 				}
 			}			
 		}
-		visited[current_node] ++;
-		
+		else{
+			apq.pop();
+		}
 		cout << endl;
+		visited[current_node] ++;
+		printQ(apq);
 
 	}
 	
@@ -184,6 +200,7 @@ int main(){
 		cout << " -> " << path.front();
 		path.pop_front();
 	}
+	cout << " -> " << DESTINATION;
 	cout << endl;
 
 	while(!apq.empty()){
